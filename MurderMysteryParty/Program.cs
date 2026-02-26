d061b0b471ec7c94035ac535a915ec589bf12770
Jim Byer <jbyer@hotmail.com>
2026-02-26 09:46:19 -0700

    Refactor project to use Blazor WebAssembly; add deployment workflow, update launch settings, and clean up components.

--- MurderMysteryParty/Program.cs
+++ MurderMysteryParty/Program.cs
@@ -1,31 +1,15 @@
-using MurderMysteryParty.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MurderMysteryParty.Services;
+using MurderMysteryParty.Components;
+using System.Net.Http;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// register app services
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddSingleton<CharacterService>();
 builder.Services.AddSingleton<GameSessionService>();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
